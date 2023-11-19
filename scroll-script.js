058504c5aceda21b8b53d57a3dd9109caa73973f
Rylan Wassem <dryland28@gmail.com>
2023-11-19 03:50:27 -0500

    Update scroll-script.js

--- scroll-script.js
+++ scroll-script.js
@@ -1,24 +1,18 @@
-// Get the container and text elements
 const scrollContainer = document.getElementById('scrollContainer');
 const scrollText = document.getElementById('scrollText');
 
-// Set up event listeners for key presses
 document.addEventListener('keydown', (event) => {
   if (event.key === 'ArrowLeft') {
-    // Scroll left when the left arrow key is pressed
     scrollTextLeft();
   } else if (event.key === 'ArrowRight') {
-    // Scroll right when the right arrow key is pressed
     scrollTextRight();
   }
 });
 
-// Function to scroll the text to the left
 function scrollTextLeft() {
-  scrollContainer.scrollLeft -= 10; // Adjust as needed
+  scrollContainer.scrollLeft -= 10;
 }
 
-// Function to scroll the text to the right
 function scrollTextRight() {
-  scrollContainer.scrollLeft += 10; // Adjust as needed
+  scrollContainer.scrollLeft += 10;
 }
